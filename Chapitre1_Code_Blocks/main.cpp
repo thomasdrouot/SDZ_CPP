0d95c82e7e362e7eb816d8c616197de4cbe905a7
Thomas Drouot <drouot.thomas@gmail.com>
2014-09-20 15:32:48 +0200

    Juste avant 1er exercice Partie 1 section 5

--- Chapitre1_Code_Blocks/main.cpp
+++ Chapitre1_Code_Blocks/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int ageUtilisateur(20);
+    int monAge(25);
+    int ageTotal(0);
+
+    ageTotal = ageUtilisateur+monAge;
+    cout << "l'age cumule est : " << ageTotal << endl;
+    //string monNom("Drouot");
+    //char lettre('a');
+    //cout << "Hello world!" << endl;
+    //cout << "Mon nom est :";
+    //cout << monNom << endl;
+    //cout << "quel est ton age?";
+    //cin >> ageUtilisateur;
+    //cin.ignore();
+    //cout << "tu as donc " << ageUtilisateur << " ans."<< endl;
+    //cout << "quel est ton nom ?";
+    //getline(cin,monNom);
+    //cout << "alors bonjour " << monNom << endl;
     return 0;
 }
